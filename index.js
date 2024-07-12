3eba4cec077b28238b4acbac1ef885d14aea821a
Khushraj Rathod <khushraj.rathod@gmail.com>
2024-07-12 16:12:50 +0530

    Add a classic star

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const height = 100;
 
 setDocDimensions(width, height);
 
+const times = (f, n) => {
+  for (let i = 0; i < n; i++) f()
+}
+
 const finalLines = [];
 
 const simpleStar = () => {
@@ -19,7 +23,23 @@ const crossStar = () => {
   )
 }
 
+const classicStar = () => {
+  const t = new bt.Turtle()
+  const size = 10
+
+  t.left(90)
+  t.right(18)
+
+  times(() => {
+    t.forward(size)
+    t.right(180 - 36)
+  }, 5)
+
+  return t.lines()
+}
+
 bt.join(finalLines, simpleStar())
 bt.join(finalLines, crossStar())
+bt.join(finalLines, classicStar())
 
 drawLines(finalLines);
